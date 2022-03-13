f8bdc07ca9daf8e2f5ed0b6e6772ce6428a9fa00
agung037 <agungk878@gmail.com>
2022-03-13 13:31:13 +0700

    add function to delete and to find specific notes

--- index.js
+++ index.js
@@ -12,40 +12,8 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
-// const generateId = () => {
-//   const maxId = notes.length > 0
-//     ? Math.max(...notes.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
-
-// app.post('/api/notes', (request, response) => {
-//   const body = request.body
-
-//   if (!body.content) {
-//     return response.status(400).json({ 
-//       error: 'content missing' 
-//     })
-//   }
-
-//   const note = {
-//     content: body.content,
-//     important: body.important || false,
-//     date: new Date(),
-//     id: generateId(),
-//   }
-
-//   notes = notes.concat(note)
-
-//   response.json(note)
-// })
-
-// app.get('/api/notes', (req, res) => {
-//   res.json(notes)
-// })
 
 // GET Query using mongodb atlas
-
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
@@ -68,33 +36,36 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
     date: new Date()
   })
-
   
   note.save().then(savedNote => {
     response.json(savedNote)
   })
 
-
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
+// Query specific notes by id
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-
-  if (note) {
+  // findById is method from mongoose
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
 })
 
+
+// Deleting specific notes by id
+app.delete('/api/notes/:id', (request, response) => {
+
+  // menemukan notes yang ingin dihapus berdasarkan id
+  Note.findById(request.params.id).then(note => {
+    // setelah ketemu, hapus post tersebut
+    note.remove() 
+    // tampilkan hasilnya
+    response.json({"report" : `note with id ${request.params.id} deleted !`})
+  })
+})
+
+
+// runing server
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
